f11d4bd4c50c941117def2173749f467820bf814
Vasco Madrid <vmadrid@email.sc.edu>
2020-11-16 11:22:17 -0500

    connect firebase account with app #21

---
+++ AccountabilityPods/Pods/Target Support Files/Pods-AccountabilityPods-AccountabilityPodsUITests/Pods-AccountabilityPods-AccountabilityPodsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AccountabilityPods_AccountabilityPodsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AccountabilityPods_AccountabilityPodsUITests
+@end

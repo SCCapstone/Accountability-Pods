e643d11571b24e6bd57e6a5a33f882681081265e
jtscholz <jtscholz@email.sc.edu>
2020-12-02 20:28:07 -0500

    creating messaging swift file

---
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MessagingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MessagingViewController.swift
+//  AccountabilityPods
+//
+//  Created by SCHOLZ, JENNIFER T on 12/2/20.
+//  Copyright © 2020 CapstoneGroup. All rights reserved.
+//
+import UIKit
+import Foundation

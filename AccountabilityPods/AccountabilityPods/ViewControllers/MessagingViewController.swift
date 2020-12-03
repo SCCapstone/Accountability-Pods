3ce6f8de8a54151f2f67c4fc1d5f369889c44201
jtscholz <jtscholz@email.sc.edu>
2020-12-02 21:31:35 -0500

    starting to MessagingViewController file, designing MessagesViewController Page on storyboard

--- AccountabilityPods/AccountabilityPods/ViewControllers/MessagingViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MessagingViewController.swift
@@ -5,5 +5,24 @@
 //  Created by SCHOLZ, JENNIFER T on 12/2/20.
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+
 import UIKit
-import Foundation
+import Firebase
+import FirebaseAuth
+
+class MessagingViewController: UIViewController {
+    
+    @IBOutlet weak var errorLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    func setUpElements() {
+        errorLabel.alpha = 0 // hide error label
+    }
+
+    
+}

a3ed15e8679f5848126c49fc24dc321fcdc7e156
ktjhada <jhada@email.sc.edu>
2020-12-04 14:50:28 -0500

    Contact List/ Messaging Function

---
+++ AccountabilityPods/AccountabilityPods/ViewControllers/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  AccountabilityPods
+//
+//  Created by KAHAN-THOMAS, JHADA L on 12/4/20.
+//  Copyright © 2020 CapstoneGroup. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AccountabilityPods/AccountabilityPods/ViewControllers/MessagingViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MessagingViewController.swift
@@ -5,24 +5,64 @@
 //  Created by SCHOLZ, JENNIFER T on 12/2/20.
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
-
 import UIKit
 import Foundation
 import Firebase
 import FirebaseAuth
 
-class MessagingViewController: UIViewController {
-
-    @IBOutlet weak var errorLabel: UILabel!
-
+class MessagingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var db:Firestore!
+    var contactsA = [String]()
+    //@IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var contactTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        //loadData()
+       contactsA = ["adminUser"]
+        contactTable.delegate = self
+        contactTable.dataSource =  self
         // Do any additional setup after loading the view.
-        setUpElements()
+        //setUpElements()
     }
-    func setUpElements() {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //print("TableView setup \(contactsA.count)")
+        return contactsA.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactName", for: indexPath)
+        //let contact = contactsA[indexPath.row]
+        cell.textLabel?.text = contactsA[indexPath.row]
+        //cell.contactName.text = contact
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegue(withIdentifier: "Chat", sender: Any?.self)    }
+    
+    //for retrieving names for contacts from firestore
+    /*func loadData() {
+        db.collection("users").document(Constants.User.sharedInstance.userID).collection("CONTACTS").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                if let querySnapshot = querySnapshot {
+                    for document in querySnapshot.documents{
+                    let data = document.data()
+                    let con = data["userRef"] as? String ?? ""
+                    self.contactsA.append(con)
+                    }
+                }
+            }
+        }
+        
+    }*/
+    /*func setUpElements() {
         errorLabel.alpha = 0 // hide error label
-    }
+    }*/
 
 }

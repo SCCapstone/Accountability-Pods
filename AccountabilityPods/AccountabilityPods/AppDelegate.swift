5fbd8536d4eb5e51519ef8b8227a9ab487556ee5
vascomadrid <vmadrid0426@gmail.com>
2021-04-03 15:58:37 -0400

    edit app delegate to use document username

--- AccountabilityPods/AccountabilityPods/AppDelegate.swift
+++ AccountabilityPods/AccountabilityPods/AppDelegate.swift
@@ -21,15 +21,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         FirebaseApp.configure()
         
         //push manager for notifications
-        let userId = Constants.User.sharedInstance.userID 
+        let userId = Auth.auth().currentUser?.uid ?? "current_user_id"
         let userRef = Firestore.firestore().collection("users").whereField("uid", isEqualTo: userId)
-        print("APPLICATION userId: \(userId)")
+        print("APPDELEGATE userId: \(userId)")
         userRef.getDocuments() { (querySnapshot, err) in
         if let err = err {
             print("Error getting documents: \(err)")
         } else {
+            print("APPDELEGATE in else statement")
             for document in querySnapshot!.documents {
-                let pushManager = PushNotificationManager(userName: Constants.User.sharedInstance.userID )
+                let userN = document.get("username") as? String ?? ""
+                let pushManager = PushNotificationManager(userName: userN)
                 pushManager.registerForPushNotifications()
             }
           }

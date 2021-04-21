7fe7e26720c76114b2724fce42d6224a6186eabd
jtscholz <jtscholz@email.sc.edu>
2021-04-20 20:56:29 -0400

    Change edit to done when user is editing for both posts and skills #117

--- AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/OwnResourceVC.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/OwnResourceVC.swift
@@ -15,6 +15,7 @@ class OwnResourceVC: UIViewController {
     var userIsEditing: Bool = false
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
     var resource: Resource = Resource()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,51 +30,55 @@ class OwnResourceVC: UIViewController {
     {
         desc.text = resource.desc
         nameLabel.text = resource.name
+        doneButton.isHidden = true
     }
     
     @IBAction func editResource(_ sender: Any) {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "OnEditResource"), object: nil)
-        if(userIsEditing)
-        {
-            overrideUserInterfaceStyle = .light
-            nameEdit.isHidden = true
-            nameLabel.isHidden = false
-            userIsEditing = false
-            editButton.tintColor = .black
-            desc.isEditable = false
-            nameLabel.text = nameEdit.text
-            nameLabel.isEnabled = true
-            nameEdit.isEnabled = false
-            nameEdit.isUserInteractionEnabled = false
-            desc.layer.backgroundColor = UIColor.white.cgColor
-            db.document(resource.path).setData(["resourceName" : nameLabel.text, "resourceDesc" : desc.text]) {err in
-                if let err = err {
-                    print(err)
-                }
-            }
-            
-            
-        }
-        else
-        {
-            overrideUserInterfaceStyle = .light
-            userIsEditing = true
-            nameLabel.isHidden = true
-            editButton.tintColor = .blue
-            desc.isEditable = true
-            nameEdit.text = nameLabel.text
-            nameLabel.isEnabled = false
-            nameEdit.isEnabled = true
-            nameEdit.isUserInteractionEnabled = true
-            nameEdit.isHidden = false
-            desc.layer.cornerRadius = 10
-            desc.layer.backgroundColor = UIColor.systemGray6.resolvedColor(with: self.traitCollection).cgColor
-            nameEdit.layer.borderWidth = 0
-            nameEdit.layer.cornerRadius = 5
-            //nameEdit.layer.backgroundColor = UIColor.systemGray6.cgColor
-        }
-    
+        overrideUserInterfaceStyle = .light
+        userIsEditing = true
+        nameLabel.isHidden = true
+        
+        editButton.isHidden = true
+        doneButton.isHidden = false
+        //edit button blue
+        
+        desc.isEditable = true
+        nameEdit.text = nameLabel.text
+        nameLabel.isEnabled = false
+        nameEdit.isEnabled = true
+        nameEdit.isUserInteractionEnabled = true
+        nameEdit.isHidden = false
+        desc.layer.cornerRadius = 10
+        desc.layer.backgroundColor = UIColor.systemGray6.resolvedColor(with: self.traitCollection).cgColor
+        nameEdit.layer.borderWidth = 0
+        nameEdit.layer.cornerRadius = 5
     }
+    @IBAction func onDone(_ sender: Any) {
+        overrideUserInterfaceStyle = .light
+        nameEdit.isHidden = true
+        nameLabel.isHidden = false
+        userIsEditing = false
+        
+        editButton.isHidden = false
+        doneButton.isHidden = true
+        editButton.tintColor = .black
+        
+        
+        
+        desc.isEditable = false
+        nameLabel.text = nameEdit.text
+        nameLabel.isEnabled = true
+        nameEdit.isEnabled = false
+        nameEdit.isUserInteractionEnabled = false
+        desc.layer.backgroundColor = UIColor.white.cgColor
+        db.document(resource.path).setData(["resourceName" : nameLabel.text, "resourceDesc" : desc.text]) {err in
+            if let err = err {
+                print(err)
+            }
+        }
+    }
+    
     
     @IBAction func onDelete(_ sender: Any) {
         let docID = db.document(resource.path).documentID

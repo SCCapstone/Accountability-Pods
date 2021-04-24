44570a569d906e7a397c75f3496c9b88a7757066
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2021-04-24 12:13:45 -0400

    Add documentation to MyPodViewController.swift #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/MyPodViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/MyPodViewController.swift
@@ -5,45 +5,56 @@
 //  Created by MADRID, VASCO MADRID on 12/3/20.
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+//  Description: Manages the own profile view and tabs for the subviews.
 
 import UIKit
 import Firebase
 import FirebaseAuth
 
 class MyPodViewController: UIViewController {
-     //ADDED
+    // MARK: - Class Variables
+    
+    /// The firestore database
     let db = Firestore.firestore()
+    /// The current users username
     let userID = Constants.User.sharedInstance.userID;
-    //HERE
+    
+    // MARK: - UI Connectors
+     
+    /// The subview tab controller. 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    /// The subview with users own posts.
     @IBOutlet weak var MyPostsContainer: UIView!
+    /// The subview with collection of posts saved by the user.
     @IBOutlet weak var SavedPostsContainer: UIView!
+    /// The subview with a collection of the users contacts.
     @IBOutlet weak var PodGroupsContainer: UIView!
+    /// The subview with user's skill and skill creation.
     @IBOutlet weak var SkillsContainer: UIView!
+    /// The label for the users first and last name.
     @IBOutlet weak var NameLabel: UILabel!
+    /// The label for the users username
     @IBOutlet weak var usernameLabel: UILabel!
+    /// The label for the users profile description
     @IBOutlet weak var descriptionLabel: UILabel!
     
-
+    // MARK: - Set up
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // set page to only work with light mode
         overrideUserInterfaceStyle = .light
+        // set up the labels at the top of page
         setName()
-        
-        //added
-        //let users = db.collection("USERS");
-        //let userDoc = users.document(userID);
-        
-        // unti here
-        
-        // Do any additional setup after loading the view.
     }
     
+    /// Sets which subview first appears depending on which sub tab is selected
+    ///
+    /// - Parameter animated: whether animation is enabled
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // show myPosts tab when pods tab chosen
-        
+        // case relates to position of selected tab
+        // switches which view is showed depending on case
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             self.MyPostsContainer.alpha = 1;
@@ -70,23 +81,13 @@ class MyPodViewController: UIViewController {
         }
     }
   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
+    /// Set up name, username, and description label shown at the top of the page
     func setName() {
-        // setting description as invisibile (edit later!)
-        // get current userID
+        // get current userID, and database
         let uid = Constants.User.sharedInstance.userID
         let db = Firestore.firestore()
         let userRef = db.collection("users").document(uid)
-        
+        // get document for current user
         _ = userRef.addSnapshotListener() {
             document, err in
             if let err = err {
@@ -94,28 +95,35 @@ class MyPodViewController: UIViewController {
             }
             else
             {
-        
+                // gets information of database
                 let firstname = document!.get("firstname") as! String
                 let lastname = document!.get("lastname") as! String
                 let username = document!.get("username") as! String
                 let description = document!.get("description") as! String
-                   
-                    let name = firstname + " " + lastname
-                    self.NameLabel.alpha = 1;
-                    self.descriptionLabel.alpha = 1;
-                    self.NameLabel.text = name
-                    self.descriptionLabel.text = description
-                    self.usernameLabel.text = "@" + username
+                
+                // format name         
+                let name = firstname + " " + lastname
+                //  update the labels
+                self.NameLabel.alpha = 1;
+                self.descriptionLabel.alpha = 1;
+                self.NameLabel.text = name
+                self.descriptionLabel.text = description
+                self.usernameLabel.text = "@" + username
             
-        }
-        
-      
-            
+             }
         }
     }
+    
+    /// Sets keyboard to hide when screen is tapped
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    // MARK: - UI Actions
+    
+    /// Changes subview depending on which tab is selected
+    ///
+    /// - Parameter sender: the segmented control tab bar
     @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
         // controls which tab within the tab is shown
         if sender.selectedSegmentIndex == 0 {
@@ -148,7 +156,12 @@ class MyPodViewController: UIViewController {
             })
         }
     }
+     
+    /// Shows a pop up that explains how to use the messaging page.
+    ///
+    /// - Parameter sender: the tapped object
     @IBAction func helpTapped(_ sender: Any) {
+        // prepares and presents pop up
         let alertController = UIAlertController(title: "My Profile Help", message: "View and edit your own posts, view and delete saved posts, view and organize contacts, add and remove skills\nTap settings in top right to change name and description or view tutorial for more detailed information", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title:"Got it!", style: .default, handler: nil))
         self.present(alertController, animated: true)

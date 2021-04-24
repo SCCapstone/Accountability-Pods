c2898c4167ba0793f069ba27b78de24be634d1ca
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2021-04-23 20:16:21 -0400

    Add documentation for ViewController.swift #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/AccountSetup/ViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/AccountSetup/ViewController.swift
@@ -6,13 +6,20 @@
 //  Also written by Jhada Kahan-Thomas
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+//  Description: Manages view controller that shows when app is first open.
 
 import UIKit
 import SwiftUI
 import Firebase
 import FirebaseAuth
 class ViewController: UIViewController {
+    // MARK: - Class Variables
+    
+    ///  The Firesetore database
     let db = Firestore.firestore()
+    
+    // MARK: - Set up
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tryLogin();
@@ -22,14 +29,20 @@ class ViewController: UIViewController {
         leftSwipe.direction=UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(leftSwipe)
     }
-     struct ContentView: View {
+    
+    /// struct for images on page
+    struct ContentView: View {
                var body: some View {
                    Image("people")
                    .resizable()
                    .scaledToFit()
                }
            }
-
+    
+    // MARK: - Navigation
+    
+    /// Logs in user when device has stored signing in values and
+    /// calls tryLoginFromStorage() with stored information
     func tryLogin() {
         let defaults = UserDefaults.standard
         
@@ -50,12 +63,16 @@ class ViewController: UIViewController {
             tryLoginFromStorage(userID: userIDString, sessID: sessionIDString);
         }
         
-        
-        
     }
     
+    /// Finishes logging in user and updates database with device information
+    ///
+    /// - Parameters:
+    ///   - userID: user id number stored in device
+    ///   - sessID: session information stored in device
     func tryLoginFromStorage(userID: String, sessID: String)
     {
+        // gets collection of user ids
         self.db.collection("userids").getDocuments() { docs, err in
         if let err = err {
             print(err)
@@ -64,6 +81,7 @@ class ViewController: UIViewController {
         else
         {
             for doc in docs!.documents {
+                // gets doc that matches id stored in device
                 if (doc.documentID == userID)
                 {
                     if((doc.data()["sessionID"]) != nil)
@@ -71,12 +89,14 @@ class ViewController: UIViewController {
                         let storedID = doc.data()["sessionID"] as! String
                         if(storedID == sessID)
                         {
+                            // sets user information shared with other view controllers
                         Constants.User.sharedInstance.userID = doc.data()["username"] as! String
                             let uID = UUID().uuidString
                             
                             doc.reference.setData(["sessionID": uID], merge:true);
                             UserDefaults.standard.setValue(uID, forKey: "sessID")
-                        self.transitionToHome()
+                            // user logged in and enters home page
+                            self.transitionToHome()
                         }
                         else
                         {
@@ -95,10 +115,9 @@ class ViewController: UIViewController {
         }
     }
     
+    /// Transitions to home page view controller when called.
     func transitionToHome() {
         let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? UITabBarController
-        
-           
            view.window?.rootViewController = homeViewController
            view.window?.makeKeyAndVisible()
            
@@ -106,6 +125,7 @@ class ViewController: UIViewController {
 }
 extension UIViewController
 {
+    /// Manages swipes on page
     @objc func swipeAction(swipe:UISwipeGestureRecognizer)
     {
         switch swipe.direction.rawValue {
@@ -118,7 +138,9 @@ extension UIViewController
         }
     }
 }
-//gradient view code
+// MARK: - Design elements of view controller.
+
+/// Designs the gradient
 @IBDesignable class GradientView: UIView{
         var gradientLayer: CAGradientLayer {
             return layer as! CAGradientLayer

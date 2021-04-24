f838b822e620365f801ee622f817ca33d7998ee6
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2021-04-23 21:50:52 -0400

    Add documentation to the MessagingViewController.swift #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/Messaging/MessagingViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/Messaging/MessagingViewController.swift
@@ -5,6 +5,7 @@
 //  Created by SCHOLZ, JENNIFER T on 12/2/20.
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+//  Description: Manages the pages that shows user to message
 import UIKit
 import Foundation
 import Firebase
@@ -12,32 +13,41 @@ import FirebaseAuth
 
 class MessagingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // MARK: - UI Connectors
+    
+    /// The table with user profiles to message
     @IBOutlet var contactTable: UITableView!
     
+    // MARK - Class Variables
+    
+    /// The firestore database
     let db = Firestore.firestore()
+    /// Array of profile objects (class in Utilities.swift) to show in the table
     var contactsA = [Profile]()
-    //var userID = Constants.User.sharedInstance.userID;
+    /// The username of the current user
     var userID  = Constants.User.sharedInstance.userID;
     
     override func viewDidLoad() {
+        // calls view did load when a contact is added or removed
         NotificationCenter.default.addObserver(self, selector: #selector(self.loadData), name: NSNotification.Name(rawValue: "ContactsChanged"), object: nil)
         super.viewDidLoad()
+        // set page to only work with light mode
         overrideUserInterfaceStyle = .light
+        // loads data into the array of profiles
         loadData()
+        // Assign UITableViewDelegate to the contacts table
         contactTable.delegate = self
         contactTable.dataSource =  self
-        //view controller to use when presenting modal Chat view controller
-        //definesPresentationContext = true
-        //setUpElements()
     }
     
-    //get users from the database load data
+    // MARK: - Set up
+    
+    /// Load contacts and users who have messaged current user into array of profiles.
     @objc private func loadData() {
+        // clear array before it is filled
         self.contactsA.removeAll()
-        
-        // GET USERS FROM CONTACTS
+        // get users from contacts 
         let usersRef = db.collection("users").document(userID).collection("CONTACTS");
-
         usersRef.getDocuments() {(querySnapshot, err) in
             if let err = err {
                 print("Error getting users: \(err)")
@@ -51,11 +61,13 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                             if let userdocument = userdocument, userdocument.exists {
                                 // user exists in database
                                 let tempProfile = Profile()
+                                // add profile to array
                                 self.contactsA.append(tempProfile)
                                 let path = "users/" + document.documentID
                                  tempProfile.readData(database: self.db, path: path, tableview: self.contactTable)
                                 print("msgContact added: \(document.documentID)")
                                 print("msgContact array size: \(self.contactsA.count)")
+                                // reload table with new contact
                                 self.contactTable.reloadData()
                             } else {
                                 print("user \(document.documentID) does not exist")
@@ -67,7 +79,7 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                 }
             }
         }
-        // GET USERS FROM CHATS, should add users that are not in contacts
+        // get users from chats, should add users that are not in contacts.
         db.collection("Chats").whereField("users" , arrayContains: self.userID)
         .getDocuments{(querySnapshot, err) in
             if let err = err {
@@ -77,8 +89,7 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                 print("Loading Users Messages")
                 for document in querySnapshot!.documents {
                     let chatUsers = document["users"] as? Array ?? [""]
-                    
-                    // get both listed users and choose the one that is not self
+                    // get both users listed in chat and choose the one that is not current user
                     for user in chatUsers{
                         if user != self.userID && user.count > 0{
                             // check if user exists
@@ -93,6 +104,7 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                                             print("\(user) is already contact")
                                         } else {
                                             print("\(user) sent message but is not a contact")
+                                            // add user to array of contacts
                                             let path = "users/" + user
                                             let tempProfile = Profile()
                                             self.contactsA.append(tempProfile)
@@ -100,7 +112,8 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                                             self.contactTable.reloadData()
                                             
                                         }
-                                    }                                } else {
+                                    }                                
+                                } else {
                                     print("User \(user) does not exist")
                                 }
                             }
@@ -113,52 +126,74 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
             }
         }
     }
-        
+    /// Sets keyboard to hide when screen is tapped.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
 
+    // MARK: - Table functions
     
-    //setting size to num of contacts
+    /// Sets the number of rows in table determined by number of profiles in array.
+    /// 
+    /// - Parameters:
+    ///   - tableView: the contacts table
+    ///   - numberOfRowsInSection: the numnber of rows in the table
+    /// - Returns: the number of rows in the profile array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //print("contacts count: \(contactsA.count)")
         return contactsA.count
     }
     
+    /// Creates the cell in the table with the info from the profile in the profile array.
+    /// 
+    /// - Parameters:
+    ///   - tableView: the contacts table
+    ///   - cellForRowAt indexPath: the index of the row in the table that need information
+    /// - Returns: the cell to be inputted into the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        // create cell using Helpers/ContactCell.swift class
         let cell = contactTable.dequeueReusableCell(withIdentifier: "contactName") as! ContactCell
+        // get cell with given index
         let contact = contactsA[indexPath.row]
-        //print("contact id: \(contact.uid)")
+        // set the cell to have the information from the profile object
         cell.setContact(profile: contact)
-        
         return cell
     }
     
-    
+    /// Transitions to chat for tap profile in table.
+    /// 
+    /// - Parameters:
+    ///   - tableView: the contact table
+    ///   - didSelectRowAt indexPath: the index of the tapped row
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // transitions to the chat room for the selected profile
         if let presented = self.presentedViewController {
             presented.removeFromParent()
         }
         self.performSegue(withIdentifier: "ToChat", sender: Any?.self)
-        //ßself.present(ChatViewController, animated: true, completion: nil)
-        
     }
     
+    /// Prepares transition to chat and transfer profile object to chat view controller.
+    ///
+    /// - Parameters:
+    ///   - segue: the segue on the storyboard that connects the messaging page to the chat room
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //print("in prepare override func")
+        // sends tapped profile through segue to chat viewcontroller
         if segue.identifier == "ToChat" {
             let indexPath = self.contactTable.indexPathForSelectedRow
             let profile = self.contactsA[(indexPath?.row)!]
-            //print("selected profile: \(profile.uid)")
             if let dView = segue.destination as? ChatViewController {
                 dView.sendToProfile = profile
-
             }
         }
     }
     
-    //delete all messages with a specific contact
+    /// Deletes all messages from a user when cell is swiped to delete.
+    /// 
+    /// - Parameters: 
+    ///   - tableView: the contact table
+    ///   - editingStyle: the style for the swiped cell
+    ///   - indexPath: the index of the swiped cell
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         //delete your messages sent to a user
         if editingStyle == .delete {
@@ -201,7 +236,14 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
                 // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
             }
         }
+    
+    // MARK: - Pop Ups
+    
+    /// Shows a pop up that explains how to use the messaging page.
+    ///
+    /// - Parameter sender: the tapped object
     @IBAction func helpTapped(_ sender: Any) {
+        // prepares and presents the pop up
         let alertController = UIAlertController(title: "Messages Help", message: "Tap on user to view, send, and delete messages\nTo send message to a new user add them to your pod and they will appear", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title:"Got it!", style: .default, handler: nil))
         self.present(alertController, animated: true)    }

176fd422906bc6aa5ab342b8f4aa58e60e48eace
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2021-04-24 13:47:18 -0400

    Add documentation to OwnResourceVC.swift #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/OwnResourceVC.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/OwnResourceVC.swift
@@ -5,43 +5,75 @@
 //  Created by duncan evans on 12/4/20.
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+//  Description: Manages the view that displays a users resource and allows the user to edit or delete the resource.
 
 import UIKit
 import Firebase
 class OwnResourceVC: UIViewController {
+    
+    // MARK: - Class Variables
+    
+    /// The firestore database.
     let db = Firestore.firestore()
-    @IBOutlet weak var desc: UITextView!
-    @IBOutlet weak var nameEdit: UITextField!
+    /// Boolean value that changes if user is editing the resource.
     var userIsEditing: Bool = false
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var editButton: UIButton!
-    @IBOutlet weak var doneButton: UIButton!
+    /// The resource object with the post information passed from the table in which the post was tapped.
     var resource: Resource = Resource()
+    
+    // MARK: - UI Connectors
+    
+    /// The desciption of a post being viewed.
+    @IBOutlet weak var desc: UITextView!
+    /// The editable name of a post being viewed.
+    @IBOutlet weak var nameEdit: UITextField!
+    /// The uneditable name of a post being viewed.
+    @IBOutlet weak var nameLabel: UILabel!
+    /// The button that the user presses to edit the post content.
+    @IBOutlet weak var editButton: UIButton!
+    /// The button that for user to save edits.
+    @IBOutlet weak var doneButton: UIButton!
+    
+    // MARK: - Set up
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // set page to only work with light mode
         overrideUserInterfaceStyle = .light
+        // set name and description
         configureText()
-        // Do any additional setup after loading the view.
     }
+    
+    /// Sets keyboard to hide when screen is tapped.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    /// Sets name and description to values in the passed resource object.
     func configureText()
     {
         desc.text = resource.desc
         nameLabel.text = resource.name
+        // initially hide done button
         doneButton.isHidden = true
     }
     
+    // MARK: - UI Functionality
+    
+    /// Allows user to edit resource name and description with edit button is pressed.
+    ///
+    /// - Parameter sender: the edit button that was tapped
     @IBAction func editResource(_ sender: Any) {
+        // sends notification to own post display that a resource has been edited.
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "OnEditResource"), object: nil)
+        // sets interface to light mode
         overrideUserInterfaceStyle = .light
+        // change editing value
         userIsEditing = true
         nameLabel.isHidden = true
         
+        // enable correct buttons
         editButton.isHidden = true
         doneButton.isHidden = false
-        //edit button blue
         
         desc.isEditable = true
         nameEdit.text = nameLabel.text
@@ -49,12 +81,17 @@ class OwnResourceVC: UIViewController {
         nameEdit.isEnabled = true
         nameEdit.isUserInteractionEnabled = true
         nameEdit.isHidden = false
+        
+        // cosmetically edit the the text views and fields
         desc.layer.cornerRadius = 10
         desc.layer.backgroundColor = UIColor.systemGray6.resolvedColor(with: self.traitCollection).cgColor
         nameEdit.layer.borderWidth = 0
         nameEdit.layer.cornerRadius = 5
     }
+    
+    /// Updates text fields and views, and the database with the edited post.
     @IBAction func onDone(_ sender: Any) {
+        // Update text fields to finish editing
         overrideUserInterfaceStyle = .light
         nameEdit.isHidden = true
         nameLabel.isHidden = false
@@ -64,14 +101,13 @@ class OwnResourceVC: UIViewController {
         doneButton.isHidden = true
         editButton.tintColor = .black
         
-        
-        
         desc.isEditable = false
         nameLabel.text = nameEdit.text
         nameLabel.isEnabled = true
         nameEdit.isEnabled = false
         nameEdit.isUserInteractionEnabled = false
         desc.layer.backgroundColor = UIColor.white.cgColor
+        // Update resource information in the database
         db.document(resource.path).setData(["resourceName" : nameLabel.text, "resourceDesc" : desc.text]) {err in
             if let err = err {
                 print(err)
@@ -79,21 +115,17 @@ class OwnResourceVC: UIViewController {
         }
     }
     
-    
+    /// Deletes resource from database when delete button is pressed.
+    ///
+    /// - Parameter sender: the delete button that was pressed
     @IBAction func onDelete(_ sender: Any) {
+        // delete the document from the database
         let docID = db.document(resource.path).documentID
         db.collection("users").document(Constants.User.sharedInstance.userID).collection("POSTEDRESOURCES").document((docID)).delete()
+        // send notication to the users own post resource display that a resources have changed
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "OnEditResource"), object: nil)
+        // close the opened display because post has been deleted
         self.dismiss(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

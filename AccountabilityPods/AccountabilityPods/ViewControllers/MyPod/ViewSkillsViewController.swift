37d1dc5062f5345a1b9e19d128ec234b8673ca57
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2021-04-24 16:17:50 -0400

    Add documentation to ViewSkillsViewController.swift #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/ViewSkillsViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/MyPod/ViewSkillsViewController.swift
@@ -5,38 +5,60 @@
 //  Created by MADRID, VASCO MADRID on 12/3/20. Code written by Duncan Evans
 //  Copyright © 2020 CapstoneGroup. All rights reserved.
 //
+//  Description: Manages table of skills in skills subview on own profile page.
 
 import UIKit
 import Firebase
 class ViewSkillsViewController: UIViewController {
+    
+    // MARK: - Class Variables
+    
+    /// Array of skill objects (see Utilities) that populate the skills table
     var skills : [Skill] = []
+    /// The Firebase database
     let db = Firestore.firestore()
+    /// The skills table 
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Set up
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Page only works in light mode
         overrideUserInterfaceStyle = .light
+        // populates skill array with skills from database
         self.generateArray()
+        // assigns table functions to skills table
         tableView.delegate = self
         tableView.dataSource = self
+        //  retrieves notification and updates table when skills are added, edited, or deleted
         NotificationCenter.default.addObserver(self, selector: #selector(self.reload), name: NSNotification.Name(rawValue: "OnEditSkill"), object: nil)
-        // Do any additional setup after loading the view.
     }
+    
+    /// Reloads table when update notification is recieved
     @objc func reload()
     {
         print("Notified")
+        // clears array of skills
         self.skills = []
-        //print("Resources cleared: \(resources)")
+        // repopulates array of skills with updated skills
         generateArray()
+        // reloads the table
         tableView.reloadData()
         
     }
+    
+    /// Hides keyboard when other spot on screen is tapped.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    /// Populates array of skills from information in database
     func generateArray()
     {
-        
+        // clear skills
         skills = []
+        // get skills from database
         db.collection("users").document(Constants.User.sharedInstance.userID).collection("SKILLS").getDocuments() {
             skillRefs,err in
             if let err = err {
@@ -46,7 +68,9 @@ class ViewSkillsViewController: UIViewController {
             {
                 for skillDoc in skillRefs!.documents {
                     let tempSkill = Skill()
+                    // add skill to array
                     self.skills.append(tempSkill)
+                    // adds database info to skill
                     tempSkill.readData(database: self.db, path: skillDoc.reference.path, tableview: self.tableView)
                     print(tempSkill.name)
                     
@@ -57,13 +81,32 @@ class ViewSkillsViewController: UIViewController {
 
 }
 extension ViewSkillsViewController: UITableViewDataSource, UITableViewDelegate {
+    // MARK: - Table functions
+    
+    /// Returns number of sections for the table.
+    ///
+    /// - Parameter tableView: the skills table
+    /// - Returns: the number of skills in the array
     func numberOfSections(in tableView: UITableView) -> Int {
-        
         return self.skills.count
     }
+    
+    /// Returns number of rows that should be in each section.
+    ///
+    /// - Parameters:
+    ///   - tableView: the skills table
+    ///   - numberOfRowsInSection section: the number of rows to be in each section
+    /// - Returns: int 1, since there should be one row per section 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    
+    /// Returns cell for given indexPath
+    ///
+    /// - Parameters:
+    ///   - tableView: the skill table
+    ///   - indexPathL the index of the cell 
+    /// - Returns: the cell to be placed in table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let skill = skills[indexPath.section]
         
@@ -75,29 +118,58 @@ extension ViewSkillsViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
         
     }
+    
+    /// Sets the height of the space between resource cells
+    ///
+    /// - Parameter:
+    ///   - tableView: the skills table
+    ///   - heightForHeaderInSection: the height of the space between cells
+    /// - Returns: the height (20) for the space between cells
     func  tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 20
     }
+    
+    /// Shows and makes cosmetic changes to space between cells
+    ///
+    /// - Parameter:
+    ///   - tableView: the skills table
+    ///   - viewForHeaderInSection section: the index of the cell
+    /// - Returns: the headerView with cosmetic changes
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
+        // changes background color of the header to be a clear space between cells
         headerView.backgroundColor = UIColor.clear
         return headerView
     }
+    
+    /// Performs transition to detailed own skill display
+    /// 
+    /// - Parameter:
+    ///   - tableView: the skill table
+    ///   - indexPath: the index of the selected row
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // perform segue in storyboard
         self.performSegue(withIdentifier: "showSkillSegue", sender: Any?.self)
+        // deselects the row after segue
         tableView.deselectRow(at: indexPath as IndexPath, animated: true)
 
     }
+    
+    /// Prepares the transition to the own skill viewer
+    ///
+    /// - Parameter: 
+    ///   - segue: the segue in the storyboard between this view and the own skill view
+    ///   - sender: the cell that was tapped
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showSkillSegue"
         {
             let indexPath = self.tableView.indexPathForSelectedRow
+            // the skill in the selected cell
             let skill = self.skills[(indexPath?.section)!]
+            // send the skill object to the own skill view
             if let dView = segue.destination as? OwnSkillViewController {
                 dView.skill = skill
             }
-           
-            
         }
         else
         {

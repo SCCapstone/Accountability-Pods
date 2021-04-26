a7e99dea46bcc34bb31cb7fa04eb5cf6062d549c
jtscholz <jtscholz@email.sc.edu>
2021-04-26 18:13:44 -0400

    adding comments

--- AccountabilityPods/AccountabilityPods/ViewControllers/OtherProfile/SettingsViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/OtherProfile/SettingsViewController.swift
@@ -12,8 +12,14 @@ import Firebase
 import FirebaseAuth
 
 class SettingsViewController: UIViewController {
+    // MARK: - Class Variables
+    
+    ///  The Firesetore database
     let db = Firestore.firestore()
     let userID = Constants.User.sharedInstance.userID;
+    // MARK: - UI Connectors
+   
+    /// The user entered email
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
@@ -23,13 +29,13 @@ class SettingsViewController: UIViewController {
 
     @IBOutlet weak var learnMorePopup: UIButton!
     @IBOutlet weak var accountIsPrivate: UISwitch!
-  //  var privateVar: Int = 0
+    // MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
+        // set page to only work with light mode
         overrideUserInterfaceStyle = .light
         setName()
-      /*  accountIsPrivate.addTarget(self, action: #selector(switchValueDidChange(_:)), for: .valueChanged)
-*/
+        // Initializes switch button to on if user is already private, off if user is not
         let docRef = db.collection("users").document(userID)
         docRef.getDocument { (document,error) in
             if let document = document, document.exists {
@@ -48,7 +54,9 @@ class SettingsViewController: UIViewController {
             
         }
     }
-
+    // MARK: Edit and Update Button Switch On Tap
+   
+    /// When edit is pressed, hides labels and shows text fields to allow a user to edit name & description
     @IBAction func onEditPressed(_ sender: Any) {
         self.editNameTextfield.alpha=1;
         self.nameLabel.alpha=0;
@@ -58,8 +66,8 @@ class SettingsViewController: UIViewController {
         self.editButton.alpha=0;
         self.editDescriptionTextview.text = descriptionLabel.text
         self.editNameTextfield.text=nameLabel.text
-        
     }
+    /// When update is pressed, hides textfields and shows updated name & description labels
     @IBAction func onUpdatePressed(_ sender: Any) {
         self.editNameTextfield.alpha=0;
         self.nameLabel.alpha=1;
@@ -67,6 +75,7 @@ class SettingsViewController: UIViewController {
         self.descriptionLabel.alpha=1;
         self.updateButton.alpha=0;
         self.editButton.alpha=1;
+        /// Updates a users description in firestore
         db.collection("users").document(userID).updateData(["description": editDescriptionTextview.text!])
         let fullname = editNameTextfield.text!
         var components = fullname.components(separatedBy: " ")
@@ -79,13 +88,15 @@ class SettingsViewController: UIViewController {
         else {
             print("Add first and last name seperated by a space")
         }
+        /// Updates a users name in firestore
         db.collection("users").document(userID).updateData(["firstname": firstName, "lastname": lastName])
         setName()
     }
-
+    
     func setName() {
         let uid = Constants.User.sharedInstance.userID
         let db = Firestore.firestore()
+        /// retrieve users collection in firestore by userID
         let userRef = db.collection("users").document(uid)
         
         _ = userRef.addSnapshotListener() {
@@ -95,11 +106,12 @@ class SettingsViewController: UIViewController {
             }
             else
             {
-                
+                /// retrieving updated name and description
                 let firstname = document!.get("firstname") as! String
                 let lastname = document!.get("lastname") as! String
                 let description = document!.get("description") as! String
                    
+                    /// labels appear on update
                     let name = firstname + " " + lastname
                     self.nameLabel.alpha = 1;
                     self.nameLabel.text = name
@@ -107,13 +119,16 @@ class SettingsViewController: UIViewController {
                     self.descriptionLabel.text = description
                     self.editButton.alpha = 1;
                 
-                //textboxes to edit dont appear yet
+                    /// textboxes to edit dont appear yet
                     self.updateButton.alpha = 0;
                     self.editNameTextfield.alpha = 0;
                     self.editDescriptionTextview.alpha = 0;
             }
         }
     }
+    // MARK: Make Account Private Button
+   
+    /// Sets user to private when switch is set to on
     @IBAction func makeAccountPrivateSwitch_Pressed(_ sender: Any) {
         if ((sender as AnyObject).isOn == true) {
             db.collection("users").document(userID).updateData(["private": 1])
@@ -121,12 +136,13 @@ class SettingsViewController: UIViewController {
                     db.collection("users").document(userID).updateData(["private": 0])
                   }
     }
+    ///Learn more button is pressed -> adds alert that provides user with info about private account functionality 
     @IBAction func learnMoreButton_Pressed(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Learn More", message: "By making your account private, no one can find or view you're profile", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title:"Got it!", style: .default, handler: nil))
         self.present(alertController, animated: true)
     }
-
+    /// Sets keyboard to hide when screen is tapped.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }

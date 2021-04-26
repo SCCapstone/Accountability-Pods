1b1326aeb17012a0dcf35e39b831feb3f9777570
vascomadrid <vmadrid0426@gmail.com>
2021-04-26 14:05:44 -0400

    add documentation to all tutorial pages, #138

--- AccountabilityPods/AccountabilityPods/ViewControllers/TutorialPages/Tutorial3ViewController.swift
+++ AccountabilityPods/AccountabilityPods/ViewControllers/TutorialPages/Tutorial3ViewController.swift
@@ -5,40 +5,43 @@
 //  Created by MADRID, VASCO MADRID on 4/1/21.
 //  Copyright © 2021 CapstoneGroup. All rights reserved.
 //
-
+//  Description: Manages third tutorial page
 import UIKit
 
 class Tutorial3ViewController: UIViewController {
 
+    // MARK: - Set up
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // page only works with light mode
         overrideUserInterfaceStyle = .light
-        // Do any additional setup after loading the view.
     }
     
+    /// Hides keyboard when user taps on other part of screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - Navigation
+    
+    /// Goes to next tutorial page
+    ///
+    /// - Parameter sender: the next button
     @IBAction func nextTapped(_ sender: Any) {
-        let nextViewController = storyboard?.instantiateViewController(identifier: "tutorial4") as? UIViewController
+        let nextViewController = storyboard?.instantiateViewController(identifier: "tutorial4")
         
         view.window?.rootViewController = nextViewController
         view.window?.makeKeyAndVisible()
     }
+    
+    /// Closes Tutorial
+    ///
+    /// - Parameter sender: the skip button
     @IBAction func skipTapped(_ sender: Any) {
         let nextViewController = storyboard?.instantiateViewController(identifier: "toHome") as? UITabBarController
         
         view.window?.rootViewController = nextViewController
         view.window?.makeKeyAndVisible()
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
